1233e2669c0ce04d4bae4348df3f7a37e02f4190
Benjamin-Harris1 <beha1503@gmail.com>
2024-03-11 17:30:26 +0100

    KINO-24 added some styling for the CinemaPage

--- src/CinemaPage/CinemaPage.tsx
+++ src/CinemaPage/CinemaPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { getCinemas } from './CinemaUtils';
 import { Cinema } from '../interfaces/interfaces';
+import basicCinemaImg from './testimgs/basiccinema.jpg'
 
 
 export default function CinemaPage(){
@@ -16,15 +17,21 @@ export default function CinemaPage(){
     }
 
     return (
-        <div>
-            <h1>Cinemas</h1>
-            <ul>
-                {cinemas.map((cinema) => (
-                    <li key={cinema.id}>
-                        <h2>{cinema.name}</h2>
-                    </li>
-                ))}
-            </ul>
+        <div className="flex flex-wrap -mx-4">
+            {cinemas.map((cinema) => (
+                <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 mb-4 flex">
+                    <div className="bg-white rounded-lg overflow-hidden shadow-lg flex flex-col w-full">
+                        <img src={basicCinemaImg} alt={cinema.name} className="w-full h-48 object-cover"/>
+                        <div className="p-4 flex flex-grow">
+                            <div className="flex flex-col justify-between flex-grow">
+                                <h2 className="text-xl font-semibold mb-2">{cinema.name}</h2>
+                                <p className="text-gray-700">{cinema.city}, {cinema.street}</p>
+                                <p className="text-gray-600 text-sm mb-4">{cinema.description}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }

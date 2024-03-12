c73d0d6182ac5eb92e217273471684b419fb0239
osman-butt <butt.osman@gmail.com>
2024-03-12 22:01:27 +0100

    Added route to schedule component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./index.css";
 import Home from "./Home";
 import Layout from "./Layout";
 import { Route, Routes } from "react-router-dom";
+import SchedulePage from "./schedule/SchedulePage";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/schedule/movie/:id" element={<SchedulePage />} />
       </Routes>
     </Layout>
   );

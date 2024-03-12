adb72cfab0dcafbebb62eac39e57af5d8313cca7
Benjamin-Harris1 <beha1503@gmail.com>
2024-03-12 10:17:30 +0100

    KINO-24 added detailed view for each cinema

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./Home";
 import Layout from "./Layout";
 import CinemaPage from "./CinemaPage/CinemaPage";
 import AdminCinemaPage from "./CinemaPage/AdminCinemaPage";
+import CinemaDetailPage from "./CinemaPage/CinemaDetailPage";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cinemas" element={<CinemaPage />} />
+        <Route path="/cinemas/:cinemaId" element={<CinemaDetailPage />} />
         <Route path="/admincinemas" element={<AdminCinemaPage />} />
       </Routes>
     </Layout>

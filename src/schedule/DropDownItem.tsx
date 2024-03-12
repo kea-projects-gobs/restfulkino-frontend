153e8f707f375b92c0733b6d696c47dd30935d8f
osman-butt <butt.osman@gmail.com>
2024-03-12 22:00:53 +0100

    Create DropDownItem.tsx

---
+++ src/schedule/DropDownItem.tsx
@@ -0,0 +1,20 @@
+import { CinemaType } from "../types";
+
+type DropDownItemProps = {
+  item: CinemaType;
+  selectItem: (item: CinemaType) => void;
+};
+
+export default function DropDownItem({ item, selectItem }: DropDownItemProps) {
+  const handleClick = () => {
+    selectItem(item);
+  };
+
+  return (
+    <li onClick={handleClick}>
+      <p className="px-4 py-2 hover:bg-gray-100 hover:cursor-pointer">
+        {item.name}
+      </p>
+    </li>
+  );
+}

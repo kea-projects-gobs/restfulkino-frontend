e59d0d2c65414095339f29eccd0092f757cc7a99
osman-butt <butt.osman@gmail.com>
2024-03-13 21:35:54 +0100

    Added API fetch

--- src/schedule/SchedulePage.tsx
+++ src/schedule/SchedulePage.tsx
@@ -3,12 +3,12 @@ import { useParams } from "react-router-dom";
 import DropDownMenu from "./DropDownMenu";
 import DatePicker from "./DatePicker";
 import TimePicker from "./TimePicker";
-import { CinemaType } from "../types";
+import { CinemaType, MovieType } from "../types";
+import MovieDescription from "./MovieDescription";
 
 export default function SchedulePage() {
-  // Get movie id from URL
   const { id } = useParams();
-  //   const [movie, setMovie] = useState<MovieType>(null);
+  const [movie, setMovie] = useState<MovieType>();
   const [cinemas, setCinemas] = useState<CinemaType[]>([]);
   const [selectedCinema, setSelectedCinema] = useState<CinemaType | null>(null);
   const [selectedScheduleDate, setSelectedScheduleDate] = useState(new Date());
@@ -19,14 +19,14 @@ export default function SchedulePage() {
   };
 
   const handleDateSelect = (date: Date) => {
-    console.log(date);
-
     setSelectedScheduleDate(date);
   };
 
-  // TODO:
-  // Fetch movie data
-  // Fetch schedule data based on selected cinema and date and movie id
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/movies/${id}`)
+      .then(res => res.json())
+      .then(data => setMovie(data));
+  }, [id]);
 
   useEffect(() => {
     fetch("http://localhost:8080/api/cinemas")
@@ -40,27 +40,18 @@ export default function SchedulePage() {
     const month = String(selectedScheduleDate.getMonth() + 1).padStart(2, "0"); // Month starts from 0
     const day = String(selectedScheduleDate.getDate()).padStart(2, "0");
     const formattedDate = `${year}-${month}-${day}`;
-
-    fetch(`http://localhost:8080/schedules/${formattedDate}`)
-      .then(res => res.json())
-      .then(data => setSchedules(data));
+    if (selectedCinema != null) {
+      fetch(
+        `http://localhost:8080/api/schedules/${formattedDate}/movies/${id}/cinemas/${selectedCinema?.id}`
+      )
+        .then(res => res.json())
+        .then(data => setSchedules(data));
+    }
   }, [selectedCinema, selectedScheduleDate, id]);
 
   return (
     <>
-      {/* Movie data component use id to fetch  */}
-      {/* Time selector component -> click event should pass the schedule id along and show seats available */}
-      <div className="flex flex-row justify-center gap-2 mx-auto mt-10">
-        {/* <div className="flex-wrap justify-center"> */}
-        <img
-          className="w-[200px] rounded-xl"
-          src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRRTP-j51D5LbT3V_xXtI7L639TWsN2KABuVeSyMRqZZDzoccDE"
-        />
-        <div className="mx-6 ">
-          <h1 className="text-xl font-bold">Kong FU Panda</h1>
-          <p className="text-sm">95 minutter</p>
-        </div>
-      </div>
+      {movie?.title && <MovieDescription movie={movie} />}
       <div className="max-w-[500px] mx-auto mt-6 flex flex-col justify-center">
         <div className="mx-auto">
           <DropDownMenu

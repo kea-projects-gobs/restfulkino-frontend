9107ce80706aa3c132e1f80a7f034be2cfbeab68
osman-butt <butt.osman@gmail.com>
2024-03-13 22:26:27 +0100

    Added link to schedule

--- src/Pages/MoviePage/Moviepage.tsx
+++ src/Pages/MoviePage/Moviepage.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { Movie } from "../../interfaces/interfaces";
 import { getMovies } from "./MovieUtils";
+import { useNavigate } from "react-router-dom";
 
 function MoviePage() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchMovies();
@@ -12,24 +14,25 @@ function MoviePage() {
   const fetchMovies = async () => {
     const response = await getMovies();
     setMovies(response.data);
-  }
+  };
 
   return (
     <div className="flex flex-wrap justify-center">
-      {movies.map((movie) => (
+      {movies.map(movie => (
         <div
-          className="mx-1 my-2 p-4 shadow-md transition duration-500 ease-in-out transform hover:scale-105 bg-slate-900 rounded-md w-80 h-[30rem] flex flex-col"
+          className="mx-1 my-2 p-4 shadow-md transition duration-500 ease-in-out transform hover:scale-105 bg-gray-100 rounded-md w-80 h-[30rem] flex flex-col hover:cursor-pointer"
           key={movie.id}
+          onClick={() => navigate(`/schedules/movies/${movie.id}`)}
         >
-          <h1 className="text-xl font-semibold mb-2 text-center text-gray-300">
+          <h1 className="mb-2 text-xl font-semibold text-center ">
             {movie.title}
           </h1>
-          <div className="flex-grow flex items-center justify-center overflow-hidden"> 
+          <div className="flex items-center justify-center flex-grow overflow-hidden">
             {movie.imageUrl && (
               <img
                 src={movie.imageUrl}
                 alt={`${movie.title} Poster`}
-                className="w-full h-full object-contain"
+                className="object-contain w-full h-full"
               />
             )}
           </div>

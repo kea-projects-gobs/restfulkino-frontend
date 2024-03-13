52d0cb25d3c1dda8bddd0694feac9a79025f8122
Benjamin-Harris1 <beha1503@gmail.com>
2024-03-13 15:07:19 +0100

    KINO-14 outcommented some future implementations to get rid of errors

--- src/Pages/FrontPage/FrontPage.tsx
+++ src/Pages/FrontPage/FrontPage.tsx
@@ -1,14 +1,16 @@
-import { useNavigate } from 'react-router-dom';
-import { Movie } from '../../interfaces/interfaces';
+// TODO's are outcommented
+
+//import { useNavigate } from 'react-router-dom';
+//import { Movie } from '../../interfaces/interfaces';
 import basiccinema from "../CinemaPage/testimgs/basiccinema.jpg"
 
 export default function TestFrontpage() {
-  const navigate = useNavigate();
+ // const navigate = useNavigate();
 
-  const handleMovieSelect = (movie: Movie) => {
-    // Redirect to the MovieDetailPage with the selected movie's ID
-    navigate(`/movies/${movie.id}`)
-  };
+  // const handleMovieSelect = (movie: Movie) => {
+  //   // Redirect to the MovieDetailPage with the selected movie's ID
+  //   navigate(`/movies/${movie.id}`)
+  // };
 
   return (
     <div className="container mx-auto px-4">
@@ -20,8 +22,6 @@ export default function TestFrontpage() {
         <h2 className="text-3xl font-semibold my-8">Now Playing</h2>
         <div className="movies-list grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {/* Movie card component goes here */}
-          <div onClick={() => handleMovieSelect({ id: 1, title: 'Movie 1' })} className="movie-card cursor-pointer bg-gray-800 text-white p-4 rounded-lg shadow-lg hover:bg-gray-700 transition duration-300 ease-in-out">Movie 1</div>
-          <div onClick={() => handleMovieSelect({ id: 2, title: 'Movie 2' })} className="movie-card cursor-pointer bg-gray-800 text-white p-4 rounded-lg shadow-lg hover:bg-gray-700 transition duration-300 ease-in-out">Movie 2</div>
         </div>
       </section>
     </div>

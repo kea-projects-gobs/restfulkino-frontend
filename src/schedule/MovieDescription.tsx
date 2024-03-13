9d6906da6e334de20b96ff89cffdf6235eacd285
osman-butt <butt.osman@gmail.com>
2024-03-13 21:35:29 +0100

    Added MovieDescription for schedule page

---
+++ src/schedule/MovieDescription.tsx
@@ -0,0 +1,24 @@
+import { MovieType } from "../types";
+
+type MovieDescriptionProps = {
+  movie: MovieType;
+};
+
+export default function MovieDescription({ movie }: MovieDescriptionProps) {
+  return (
+    <div className="flex flex-row justify-center gap-2 mx-auto mt-10">
+      {/* <div className="flex-wrap justify-center"> */}
+      <img className="w-[200px] rounded-xl" src={movie?.imageUrl} />
+      <div className="mx-6 max-w-[300px]">
+        <h1 className="text-xl font-bold">{movie?.title}</h1>
+        <p className="text-sm opacity-65">{movie?.releaseDate}</p>
+        <span className="text-sm opacity-65">{movie?.duration} minutter</span>
+        {movie.genre != null && (
+          <span className="text-sm opacity-65"> - {movie.genre}</span>
+        )}
+        <p className="text-sm opacity-65">{movie?.cast}</p>
+        <p className="mt-2 text">{movie?.description}</p>
+      </div>
+    </div>
+  );
+}

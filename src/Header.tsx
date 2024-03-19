f757f4ddfc4e2f3185670d46af5f064124232624
Kapalee <Shamvictor@hotmail.com>
2024-03-19 16:18:06 +0100

    header

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,16 +5,33 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
   const auth = useAuth();
 
-  return (
-    <header className="border-b-[1px] border-border border-solid w-full pb-4 grid grid-cols-2">
-      HEADER
-      <div className="flex justify-end gap-2">
-        <NavLink to="/">Home</NavLink>
-        {auth?.isLoggedInAs(["ADMIN"]) && (
-          <NavLink to="/admin">Admin</NavLink>
-        )}
-        <AuthStatus />
-      </div>
-    </header>
+
+return (
+  <header className="bg-gray-900 text-white rounded-lg shadow-lg">
+    <div className="container mx-auto py-6 px-6 flex justify-between items-center">
+      <h1 className="text-4xl ">CinemaApp</h1>
+      <nav>
+        <ul className="flex space-x-6">
+          <li>
+            <a href="MoviePage" className="hover:text-gray-300">
+              Program
+            </a>
+          </li>
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Coming soon
+              </a>
+            </li>
+              <AuthStatus />
+          <li>
+            <a href="#" className="hover:text-gray-300">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
   );
-};
+}
+

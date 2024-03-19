acd2a1a5615b12600325e210d7451da8a1da6bc7
Kapalee <Shamvictor@hotmail.com>
2024-03-19 16:34:18 +0100

    Updated header

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,33 +5,38 @@ import { NavLink } from "react-router-dom";
 export const Header = () => {
   const auth = useAuth();
 
-
-return (
-  <header className="bg-gray-900 text-white rounded-lg shadow-lg">
-    <div className="container mx-auto py-6 px-6 flex justify-between items-center">
-      <h1 className="text-4xl ">CinemaApp</h1>
-      <nav>
-        <ul className="flex space-x-6">
-          <li>
-            <a href="MoviePage" className="hover:text-gray-300">
-              Program
-            </a>
-          </li>
+  return (
+    <header className="bg-gray-900 text-white rounded-lg shadow-lg">
+      <div className="container mx-auto py-6 px-6 flex justify-between items-center">
+        <h1 className="text-4xl ">CinemaApp</h1>
+        <nav>
+          <ul className="flex space-x-6">
             <li>
-              <a href="#" className="hover:text-gray-300">
-                Coming soon
-              </a>
+              <NavLink to="/" className="hover:text-gray-300">
+                Program
+              </NavLink>
             </li>
-              <AuthStatus />
-          <li>
-            <a href="#" className="hover:text-gray-300">
-              About
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  </header>
+            <li>
+              <NavLink to="/comingsoon" className="hover:text-gray-300">
+                Coming soon
+              </NavLink>
+            </li>
+            <AuthStatus />
+            <li>
+              <NavLink to="/about" className="hover:text-gray-300">
+                About
+              </NavLink>
+            </li>
+            {auth?.isLoggedInAs(["ADMIN"]) && (
+              <li>
+                <NavLink to="/admin" className="hover:text-gray-300">
+                  Admin
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
-}
-
+};

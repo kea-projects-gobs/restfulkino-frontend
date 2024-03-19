11f9968083a8368a59c84b39f9aee64667dd67cd
Kapalee <Shamvictor@hotmail.com>
2024-03-19 12:08:51 +0100

    Update Header.tsx

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,5 +1,45 @@
-export const Header = () => (
-  <header className="border-b-[1px] border-border border-solid w-full pb-4 grid grid-cols-2">
-    HEADER
+import "./index.css";
+
+
+
+export const Header = ({ isLoggedIn, onLogin, onLogout }) => (
+  <header className="bg-gray-900 text-white rounded-lg shadow-lg">
+    <div className="container mx-auto py-6 px-6 flex justify-between items-center">
+      <h1 className="text-4xl ">CinemaApp</h1>
+      <nav>
+        <ul className="flex space-x-6">
+          <li>
+            <a href="MoviePage" className="hover:text-gray-300">
+              Program
+            </a>
+          </li>
+          {!isLoggedIn && (
+            <li>
+              <a href="#" className="hover:text-gray-300">
+                Coming soon
+              </a>
+            </li>
+          )}
+          {isLoggedIn ? (
+            <li>
+              <a href="#" onClick={onLogout} className="hover:text-gray-300">
+                Logout
+              </a>
+            </li>
+          ) : (
+            <li>
+              <a href="#" onClick={onLogin} className="hover:text-gray-300">
+                Login
+              </a>
+            </li>
+          )}
+          <li>
+            <a href="#" className="hover:text-gray-300">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 );

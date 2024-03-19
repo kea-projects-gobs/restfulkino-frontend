7ea9d7580aff13d993218ced3a505895fe7900e9
Benjamin-Harris1 <beha1503@gmail.com>
2024-03-19 19:46:00 +0100

    KINO-73 conditional rendering on adminpage based on if logged in as ADMIN (can do everything) or EMPLOYEE (can not do everything)

--- src/Pages/admin/AdminPage.tsx
+++ src/Pages/admin/AdminPage.tsx
@@ -3,13 +3,18 @@ import { CinemaManager } from './CinemaManager';
 import { HallManager } from './HallManager';
 import { MovieManager } from './MovieManager';
 import { ScheduleManager } from './ScheduleManager';
+import { useAuth } from '../../security/AuthProvider';
 
 export default function AdminPage() {
+  const auth = useAuth();
   const [activeTab, setActiveTab] = useState<'cinemas' | 'halls' | 'movies' | 'schedules'>('cinemas');
+
 
   return (
     <div>
       <div className="mb-4 flex flex-wrap gap-4 max-w-5xl m-auto justify-center sm:flex-nowrap">
+        {auth?.isLoggedInAs(["ADMIN"]) && (
+      <>
         <button
           onClick={() => setActiveTab('cinemas')}
           className={`flex-1 py-2 px-4 font-bold ${activeTab === 'cinemas' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'} rounded-l`}
@@ -22,6 +27,10 @@ export default function AdminPage() {
         >
           Manage Halls
         </button>
+      </>
+        )}
+        {auth?.isLoggedInAs(["ADMIN","EMPLOYEE"]) && (
+      <>
         <button
         onClick={() => setActiveTab('movies')}
         className={`flex-1 py-2 px-4 font-bold ${activeTab === 'movies' ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'} rounded-r`}
@@ -34,6 +43,8 @@ export default function AdminPage() {
         >
         Manage Schedules
         </button>
+        </>
+        )}
       </div>
 
       {activeTab === 'cinemas' && <CinemaManager />}

be532e78269f92d3a88da3c6053cd8bc4af13f9b
Benjamin-Harris1 <beha1503@gmail.com>
2024-03-21 19:06:31 +0100

    KINO-49 carousel shows movie img now

--- src/Pages/home/Carousel.tsx
+++ src/Pages/home/Carousel.tsx
@@ -3,24 +3,20 @@ import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 import { RxDotFilled } from "react-icons/rx";
 import { getMovies } from "../movie/MovieUtils";
 import { Movie } from "../../interfaces/interfaces";
-
+import { useNavigate } from "react-router";
 
 function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [slides, setSlides] = useState<Movie[]>([]);
-  const [movies, setMovies] = useState <Movie[]> ([])
-  const fetchMovies = async () => {
-  const response = await getMovies()
-  setMovies(response.data)
-  console.log(response.data);
-  
-}
+  const navigate = useNavigate();
 
-useEffect(() => {fetchMovies()}, [])
-useEffect(() => {
-  setSlides(movies);
-}, [movies]);
-
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await getMovies();
+      setSlides(response.data);
+    };
+    fetchMovies();
+  }, []);
 
   const prevSlide = () => {
     const isFirstSlide = currentIndex === 0;
@@ -34,36 +30,35 @@ useEffect(() => {
     setCurrentIndex(newIndex);
   };
 
-  const goToSlide = (slideIndex) => {
-    setCurrentIndex(slideIndex);
+  const goToSlide = (index: number) => {
+    setCurrentIndex(index);
   };
 
   return (
-  
     <div className="max-w-[1440px] h-[600px] w-full m-auto  px-4 relative group">
-      <div>
-      {slides.map((movie) => {
+      {slides.map((movie, index) => {
         return (
           <div
             key={movie.id}
-            className="w-full h-full rounded-2xl bg-center bg-cover duration-500"
+            className={`w-full h-full rounded-2xl bg-center bg-contain duration-500 ${currentIndex === index ? "block" : "hidden"}`}
             style={{ backgroundImage: `url(${movie.imageUrl})` }}
+            onClick={() => navigate(`schedules/movies/${movie.id}`)}
           ></div>
         );
       })}
-      </div>
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactLeft onClick={prevSlide} size={30} />
       </div>
-
       <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
-
-      <div className="flex top-4 justify-center py-2">
-        {movies.map((_, slideIndex) => (
-          <div key={slideIndex} onClick ={() => goToSlide(slideIndex)} 
-          className="text-2xl cursor-pointer">
+      <div className="flex justify-center py-2">
+        {slides.map((_, index) => (
+          <div
+            key={index}
+            className={`mx-1 text-2xl cursor-pointer ${currentIndex === index ? 'text-white' : 'text-gray-400'}`}
+            onClick={() => goToSlide(index)}
+          >
             <RxDotFilled />
           </div>
         ))}
